8a70f47ddb245f7f6d29f553b298963534786932
Tuanzeebee <tuanzeebee@gmail.com>
2025-04-29 14:39:54 +0700

    git init first commit login register ,home page blog fontend or backend food requiremnt system

--- Spring Boot/src/main/java/com/tuanzeebee/springboot/demosecurity/repository/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tuanzeebee.springboot.demosecurity.repository;
-
-import com.tuanzeebee.springboot.demosecurity.entity.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientRepository extends JpaRepository<Ingredient, Long>{
-}
--- Spring Boot/src/main/java/com/tuanzeebee/springboot/demosecurity/repository/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tuanzeebee.springboot.demosecurity.repository;
-
-import com.tuanzeebee.springboot.demosecurity.entity.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
